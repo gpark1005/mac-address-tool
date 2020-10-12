9c5a6a13e95027096e3b3627fa551449cd2ba284
Gerald Parker <gpark1005@gmail.com>
2020-10-12 00:23:06 +0000

    Accept 12-digit mac address with no delimeters

--- internal/params/options.go
+++ internal/params/options.go
@@ -12,12 +12,23 @@ type Options struct {
 }
 
 func (o Options) Valid() error {
+
+	// Add delimiters if needed
+	addr := o.MacAddress
+	if len(addr) == 12 {
+		for i := 2; i < len(addr); i += 3 {
+			addr = addr[:i] + ":" + addr[i:]
+		}
+	}
+
+	// Validate MAC address
 	_, err := net.ParseMAC(o.MacAddress)
 	if err != nil {
 		err := errors.New("Please enter a valid mac address")
 		return err
 	}
 
+	// Validate format argument
 	if o.Format != "" && o.Format != "json" && o.Format != "xml" && o.Format != "csv" {
 		err := errors.New("Please enter a valid return format, for example: json")
 		return err
